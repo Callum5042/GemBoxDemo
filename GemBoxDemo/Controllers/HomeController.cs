23e68bdf6c5435477b191079845da74d7c0b8549
Callum Anning <Callum5042@gmail.com>
2023-01-23 13:20:54 +0000

    Added mailmerge

--- GemBoxDemo/Controllers/HomeController.cs
+++ GemBoxDemo/Controllers/HomeController.cs
@@ -40,5 +40,50 @@ namespace GemBoxDemo.Controllers
 
             return File(stream.ToArray(), SaveOptions.DocxDefault.ContentType, "GemBoxDemo.docx");
         }
+
+        public IActionResult DownloadPdf()
+        {
+            var document = new DocumentModel();
+
+            var section = new Section(document);
+            document.Sections.Add(section);
+
+            var paragraph = new Paragraph(document);
+            section.Blocks.Add(paragraph);
+
+            var run = new Run(document, "Hello World!");
+            paragraph.Inlines.Add(run);
+
+            using var stream = new MemoryStream();
+            // document.Save("Hello World.docx");
+            document.Save(stream, SaveOptions.PdfDefault);
+
+            return File(stream.ToArray(), SaveOptions.PdfDefault.ContentType, "GemBoxDemo.pdf");
+        }
+
+        public IActionResult DownloadMailMerge()
+        {
+            //var doc = new DocumentModel();
+
+            //// Add document content.
+            //doc.Sections.Add(new Section(doc, new Paragraph(doc, new Field(doc, FieldType.MergeField, "FullName"))));
+
+            //// Save the document to a file.
+            //doc.Save("TemplateDocument.docx");
+
+            var doc = DocumentModel.Load(@"D:\Sources\Testing\GemBoxDemo\GemBoxDemo\template.docx");
+
+            // Initialize mail merge data source.
+            var dataSource = new { FullName = "John Doe" };
+
+            // Execute mail merge.
+            doc.MailMerge.Execute(dataSource);
+
+            // Save
+            using var stream = new MemoryStream();
+            doc.Save(stream, SaveOptions.DocxDefault);
+
+            return File(stream.ToArray(), SaveOptions.DocxDefault.ContentType, "GemBoxDemo.docx");
+        }
     }
 }
