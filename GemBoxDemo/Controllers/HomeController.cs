a701e11f3fe84b0a78f662cb72f7ee75a5c7f781
Callum Anning <Callum5042@gmail.com>
2023-01-23 12:56:11 +0000

    Added GemBox Document

--- GemBoxDemo/Controllers/HomeController.cs
+++ GemBoxDemo/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using GemBoxDemo.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using GemBox.Document;
 
 namespace GemBoxDemo.Controllers
 {
@@ -11,11 +12,33 @@ namespace GemBoxDemo.Controllers
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+
+            ComponentInfo.SetLicense("FREE-LIMITED-KEY");
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Download()
+        {
+            var document = new DocumentModel();
+
+            var section = new Section(document);
+            document.Sections.Add(section);
+
+            var paragraph = new Paragraph(document);
+            section.Blocks.Add(paragraph);
+
+            var run = new Run(document, "Hello World!");
+            paragraph.Inlines.Add(run);
+
+            using var stream = new MemoryStream();
+            // document.Save("Hello World.docx");
+            document.Save(stream, SaveOptions.DocxDefault);
+
+            return File(stream.ToArray(), SaveOptions.DocxDefault.ContentType, "GemBoxDemo.docx");
         }
     }
 }
