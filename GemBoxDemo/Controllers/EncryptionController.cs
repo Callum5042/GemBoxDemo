9c28222f821f0bb34c308f340b4e24aba73d9712
Callum Anning <callum.anning@parmenion.co.uk>
2023-01-26 14:44:54 +0000

    Encryption

---
+++ GemBoxDemo/Controllers/EncryptionController.cs
@@ -0,0 +1,44 @@
+﻿using GemBox.Document;
+using GemBoxDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GemBoxDemo.Controllers
+{
+    public class EncryptionController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new EncryptionModel();
+            return View(model);
+        }
+
+        public IActionResult Download(EncryptionModel model)
+        {
+            // Set License
+            ComponentInfo.SetLicense("FREE-LIMITED-KEY");
+
+            // Create document
+            var document = new DocumentModel();
+
+            // Paragraph section
+            var section = new Section(document);
+            document.Sections.Add(section);
+
+            // Paragraph
+            var paragraph = new Paragraph(document, model.Content);
+            section.Blocks.Add(paragraph);
+
+            // Save options
+            var options = new PdfSaveOptions()
+            {
+                DocumentOpenPassword = model.Password,
+                Permissions = PdfPermissions.None
+            };
+
+            // Save
+            using var stream = new MemoryStream();
+            document.Save(stream, options);
+            return File(stream.ToArray(), SaveOptions.PdfDefault.ContentType, "GemBoxEncryptionDemo.pdf");
+        }
+    }
+}
---
+++ GemBoxDemo/Models/EncryptionModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GemBoxDemo.Models
+{
+    public class EncryptionModel
+    {
+        public string Content { get; set; } = string.Empty;
+
+        public string? Password { get; set; }
+    }
+}
