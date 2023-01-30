31ebcc7bd354bbbe4c81b0352f3e7f276b09b464
Callum Anning <callum.anning@parmenion.co.uk>
2023-01-30 11:01:18 +0000

    Added ranges page

---
+++ GemBoxDemo/Controllers/RangesController.cs
@@ -0,0 +1,29 @@
+﻿using GemBoxDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GemBoxDemo.Controllers
+{
+    public class RangesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = GenerateModel();
+            return View(model);
+        }
+
+        private static RangesModel GenerateModel()
+        {
+            return new RangesModel
+            {
+                Movies = new List<MovieListModel>()
+                {
+                    new MovieListModel() { Title = "Interstellar", ReleaseYear = new DateTime(2014, 1, 1) },
+                    new MovieListModel() { Title = "The Shawshank Redemption", ReleaseYear = new DateTime(1994, 1, 1) },
+                    new MovieListModel() { Title = "The Godfather", ReleaseYear = new DateTime(1972, 1, 1) },
+                    new MovieListModel() { Title = "Gladiator", ReleaseYear = new DateTime(2000, 1, 1) },
+                    new MovieListModel() { Title = "The Prestige", ReleaseYear = new DateTime(2006, 1, 1) },
+                }
+            };
+        }
+    }
+}
---
+++ GemBoxDemo/Models/RangesModel.cs
@@ -0,0 +1,14 @@
+﻿namespace GemBoxDemo.Models
+{
+    public class RangesModel
+    {
+        public IList<MovieListModel> Movies { get; set; } = new List<MovieListModel>();
+    }
+
+    public class MovieListModel
+    {
+        public string? Title { get; set; }
+
+        public DateTime? ReleaseYear { get; set; }
+    }
+}
